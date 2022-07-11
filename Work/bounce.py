b06a3d19478e4ad356ef03f13cf323de8aec6e29
jkngg <george.jecan@gmail.com>
2022-07-11 12:41:36 +0300

    Solved bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100.0
+jump_portion = 3/5
+counter = 0
+
+while counter <= 9:
+    height *= jump_portion
+    counter += 1
+    print(counter, round(height, ndigits=4))
